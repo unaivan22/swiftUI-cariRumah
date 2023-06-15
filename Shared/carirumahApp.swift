3d3a133d4a1dd40c10361848bfa5e3446696a78c
unaivan22 <unaivan22@gmail.com>
2023-06-15 18:26:11 +0700

    Initial Commit

---
+++ Shared/carirumahApp.swift
@@ -0,0 +1,17 @@
+//
+//  carirumahApp.swift
+//  Shared
+//
+//  Created by unaivan on 15/06/23.
+//
+
+import SwiftUI
+
+@main
+struct carirumahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
